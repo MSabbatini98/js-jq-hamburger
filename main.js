ec8bf39a150a3fd53a53ebf18e18797a2fe9d095
MSebb23 <matteosabbatini23@gmail.com>
2021-03-12 09:58:16 +0100

    jquery added

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-var x = $('.close');
-// var menu = $('');
-/*
-$('hamburger-menu').click(
-
-    function () {
-
+var hamburger = $('.hamburger-menu');
+var ham_btn = $('.fa-bars');
+var close_btn = $('.close');
+ham_btn.click(
+    function(){
+        hamburger.addClass('active')
     }
-
 );
-*/
+
+close_btn.click(
+    function(){
+        ham_btn.removeClass('active')
+    }
+);
